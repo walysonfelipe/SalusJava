33fcc8fa606d181bd886da938ef725a69f5224d0
Walyson Assis <walysonfelipe25@gmail.com>
2026-03-26 23:44:14 -0300

    feat: Remove the entire V2 GUI implementation, its services, UI utilities, and associated documentation.

--- src/v2/telas/Navegador.java
+++
@@ -1,9 +0,0 @@
-package v2.telas;
-
-/**
- * Interface de navegação entre telas.
- * Implementada por SalusSystem para desacoplar as telas do JFrame principal.
- */
-public interface Navegador {
-    void irPara(String tela);
-}
